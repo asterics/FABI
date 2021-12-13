601030c642c3ba151f341790021238dcdecf4abe
Chris Veigl <veigl@technikum-wien.at>
2021-12-13 20:54:34 +0100

    added tempered scale frequencies

--- FabiWare/toneFABI.cpp
+++ FabiWare/toneFABI.cpp
@@ -1,4 +1,3 @@
-
 /* 
      Flexible Assistive Button Interface (FABI) - AsTeRICS Foundation - http://www.asterics-foundation.org
      for controlling HID functions via momentary switches and/or serial AT-commands  
@@ -17,6 +16,7 @@
 
 
 static volatile uint32_t ovf_counter = 0;
+uint16_t freqtable[] = {523, 587, 659, 698, 784, 880, 988, 1047};
 
 /**
 	@name ISR_TIMER3_COMPA
@@ -61,6 +61,10 @@ void toneFABI(uint16_t frequency, uint16_t time)
 {
   //calculate the count of overflows, which is the frequency in [Hz]
   //multiplied by the time it should run, but in [ms] -> divide by 1k
+  
+  if ((frequency>0) && (frequency<=8))   // select one note of the tempered scale
+     frequency = freqtable[frequency-1];
+  
 	uint32_t temp = ((uint32_t)frequency * (uint32_t)time) / 1000;
   //enclose the copy in an atomic block
   cli();
@@ -70,12 +74,9 @@ void toneFABI(uint16_t frequency, uint16_t time)
   //divide by the desired frequency to get the timer ticks
   OCR3A = (uint16_t)((uint32_t)250000 / (uint32_t)frequency); 
   OCR3B = OCR3A / 2; //50% DC
-  //set FastPWM mode (TOP -> OCR3A), enable prescaler 8
+  //set FastPWM mode (TOP -> OCR3A), enable prescaler
   TCCR3B = (1<<WGM33)|(1<<WGM32)|(1<<CS31)|(1<<CS30);
 }
-
-uint8_t beepCounter = 0;    // number of beeps
-uint16_t beepTime = BEEP_duration * 2;     // duration of beep and following same times without sound
 
 /**
    @name initBuzzer
@@ -89,5 +90,5 @@ void initBuzzer() {
   TIMSK3 = (1<<OCIE3A)|(1<<OCIE3B);
   TCCR3A = (1<<WGM31)|(1<<WGM30);
   sei();
-  toneFABI(800,100);
+  toneFABI(1,100);
 }
